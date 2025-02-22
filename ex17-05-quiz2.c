c69aafb06179cf07b2780cfa55d4a29686dd91e4
dobby2026 <dobbyisfree2026@gmail.com>
2025-02-22 15:08:40 +0900

    Add files via upload

---
+++ ex17-05-quiz2.c
@@ -0,0 +1,25 @@
+/*
+파일명: ex17-05-quiz2.c
+
+quiz2 
+배열의 요소를 한칸씩 오른쪽으로 이동시키는 함수를 작성하세요
+마지막 요소는 첫 번째 위치로 이동합니다
+포인터 연산을 사용해야 합니다
+*/
+
+#include <stdio.h>
+
+void rotateRight(int *arr, int size) 
+{
+    // 이 부분을 완성하세요
+}
+
+int main(void) 
+{
+    int number[5] = {1, 2, 3, 4, 5};
+    rotateRight(number, 5);
+
+    // number 배열 순차적 출력(printf) 완성하세요
+
+    return 0;
+}
