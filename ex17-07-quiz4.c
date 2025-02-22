960d446bc203e0420929be2b56579935db8cfdfd
dobby2026 <dobbyisfree2026@gmail.com>
2025-02-22 15:34:08 +0900

    Add files via upload

---
+++ ex17-07-quiz4.c
@@ -0,0 +1,20 @@
+/*
+파일명: ex17-07-quiz4.c
+양의 정수를 입력받아 각 자릿수를 뒤집어 출력하는 재귀함수를 작성하세요
+예) 1234 -> 4321
+*/
+#include <stdio.h>
+
+void reverseNumber(int n)
+{
+    // 이 부분을 완성하세요
+
+}
+
+int main(void)
+{
+    int num = 1234;
+    reverseNumber(num);
+
+    return 0;
+}
